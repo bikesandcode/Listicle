be59340b18e050cef07f3dc3d8fa66da39fea1bc
Warren Taylor <wtaylor@pcis.com>
2011-12-05 12:43:27 -0800

    Motion around working. Time to generalize a little bit.

--- jquery.listicle.js
+++ jquery.listicle.js
@@ -15,19 +15,40 @@
 /*jslint devel: true, browser: true, white: true, maxerr: 50, indent: 2, unparam: true */
 /*global jQuery : false, $ : false */
 
-(function () {
+//(function () {
   "use strict";
   var $ = jQuery, 
       nullity, 
       isArray,
+      removeById,
       buildList,
-      animate;
+      animate,
+      idIfy,
+      unIdIfy,
+      handleInputList,
+      firstList = [], secondList = [];
 
   //Crockford friendly
   nullity = function( thing ){ return thing === undefined || thing === null; };
 
   //http://ajaxian.com/archives/isarray-why-is-it-so-bloody-hard-to-get-right
   isArray = function( thing ){ return Object.prototype.toString.call(thing) === '[object Array]'; };
+  
+  //Turn a string into an id to be used in the lists.
+  idIfy = function( string ){ return "listicleElement-" + string;};  
+  
+  //and convert back to the actual 'id' of the element
+  unIdIfy = function( idString ){ return idString.substr("listicleElement-".length); };
+  
+  //remove from the provided list given the id property
+  removeById = function( array, id ){
+    var foundAt = -1;
+    $.each(array, function(index){ 
+      if( array[index].id === id ){ foundAt = index; }
+    });
+    
+    return array.splice(foundAt, 1);
+  };
 
   //basic simple animate function as an example
   animate = function( li, destinationUl ){
@@ -37,23 +58,25 @@
     });
   };
 
-  buildList = function( sourceList, otherList, list ){
-    $.each(list, function(index, value){
-      var listObject = null, toAppend = $("<li></li>");
-      if( typeof(value) === "string" ){ listObject = { id : value, text : value }; }
-      else{ listObject = value; }
-
-      toAppend.attr("id", "listicleElement-" + listObject.id);
-      toAppend.text(listObject.text);
-      sourceList.append(toAppend);
-    });
-
-    sourceList.find("li").click(function(){
-      //'move' the thing
-      animate($(this), otherList);
+  buildList = function( listUl, objectList ){
+    $.each(objectList, function(index, value){
+      var toAppend = $("<li></li>");
+      toAppend.attr("id", idIfy(value.id));
+      toAppend.text(value.text);
+      listUl.append(toAppend);
     });
   };
 
+  handleInputList = function( input ){
+    var destination = [];
+    $.each(input, function(index, value){
+      var listObject = null;
+      if( typeof(value) === "string" ){ listObject = { id : value, text : value }; }
+      else{ listObject = value; }
+      destination.push(listObject);
+    });
+    return destination;
+  };
 
   $.fn.listicle = function(list1, list2){
     var root = $(this), list1Element, list2Element;
@@ -64,16 +87,50 @@
     //basic usage checks
     if( nullity(list1) || nullity(list2) ){ throw "Neither lists can be nullish, both must be present" ;}
     if( !isArray(list1) || !isArray(list2) ){ throw "Both lists must actually be arrays."; }
+
+    //setup data
+    firstList = handleInputList( list1 );
+    secondList = handleInputList( list2 );
     
     //setup lists
-    root.append($("<ul></ul>").addClass("listicle-list1").addClass("listicle"));
-    list1Element = root.find(".listicle-list1");    
+    root.append($("<ul></ul>").addClass("listicle-firstList").addClass("listicle"));
+    list1Element = root.find(".listicle-firstList");    
 
-    root.append($("<ul></ul>").addClass("listicle-list2").addClass("listicle"));
-    list2Element = root.find(".listicle-list2");
+    root.append($("<ul></ul>").addClass("listicle-secondList").addClass("listicle"));
+    list2Element = root.find(".listicle-secondList");
 
-    buildList(list1Element, list2Element, list1);
-    buildList(list2Element, list1Element, list2);
-
+    buildList(list1Element, firstList);
+    buildList(list2Element, secondList);
+    
+    root.find("li").click(function(){
+      var id = $(this).attr("id"),
+          inList = secondList,
+          otherList = firstList,
+          listObject = null;
+      
+      //find which list it's in
+      $.each(firstList, function(index, value){
+        if( value.id === unIdIfy(id) ){ 
+          inList = firstList; 
+          otherList = secondList;
+        }
+      });
+      
+      //move it to the other
+      listObject = removeById(inList, unIdIfy(id))[0];
+      otherList.push(listObject);
+      
+      //and move the DOM element
+      if( inList === firstList ){ animate($(this), $(".listicle-secondList")); }
+      else{ animate($(this), $(".listicle-firstList")); }
+      
+      var list1Out = "", list2Out = "";
+      $.each(firstList, function(index, value){list1Out += value.id + ",";});
+      $.each(secondList, function(index, value){list2Out += value.id + ",";});
+      
+      console.info(list1Out);
+      console.info(list2Out);
+    });
+    
   };
-}());
+//}());
